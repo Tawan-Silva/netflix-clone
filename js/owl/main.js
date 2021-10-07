96b53a4bbda2c9c825018b84f0544ebba842f841
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2021-10-07 16:32:21 -0300

    HTML Web Developer

---
+++ js/owl/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2.5
+        },
+        768: {
+            items: 4.5
+        },
+        1024: {
+            items: 5.5
+        },
+        1280: {
+            items: 7.5
+        },
+        1440: {
+            items: 10.5
+        }
+    }
+})
